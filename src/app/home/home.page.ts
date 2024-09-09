4e9d4ee5da5ade20708ad2219330317cc0b61873
achiraya1 <khwaxchirya38@gmail.com>
2024-09-09 12:42:59 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private detail:NavController) {}
 
+
+Gotodetail(i:any){
+  this.detail.navigateForward('/detail', { state:{i}} );
+}
+
+  items=[
+    {
+      id:1,
+      name:"iPhone 15 Pro Max",
+      price:39900,
+      detail:"The is a iPhone 15 Pro Max by Apple",
+      imagesURL:"assets/img/Product/i15promax 1.png"
+    },
+    {
+      id:2,
+      name:"Samsung S 24 Ultar",
+      price:34900,
+      detail:"The is a Samsung S 24 Ultar by Samsung",
+      imagesURL:"assets/img/Product/s24u 1.png"
+    },
+    {
+      id:3,
+      name:"Samsung A54 5G",
+      price:19900,
+      detail:"The is a Samsung A54 5G by Samsung",
+      imagesURL:"assets/img/Product/A54 1.png"
+    },
+    {
+      id:4,
+      name:"Xiaomi 14T Pro ",
+      price:19900,
+      detail:"The is a Xiaomi 14T Pro by Xiaomi",
+      imagesURL:"assets/img/Product/mi14tpro 1.png"
+    },
+  ]
 }
